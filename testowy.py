77b867b43dcf416820ca892c5aa53cb41e41b62e
Krystian Hetmanek <tquboid@gmail.com>
2022-06-23 02:39:33 +0200

    Add testowy.py

---
+++ testowy.py
@@ -0,0 +1,14 @@
+zakupy={
+  'piekarnia': ['chleb', 'bułki', 'pączek'],
+  'warzywniak': ['marchew', 'seler', 'rukola'],
+}
+
+zakupy = {k:[x.capitalize() for x in v] for k,v in zakupy.items()}
+for key, value in zakupy.items():
+    print('Idę do', key.title(),'i kupuję tam', value,".")
+
+length_piekarnia=len(zakupy['piekarnia'])
+length_warzywniak=len(zakupy['warzywniak'])
+length_suma=length_piekarnia+length_warzywniak
+print("W sumie kupuję", length_suma,"produktów")
+    
